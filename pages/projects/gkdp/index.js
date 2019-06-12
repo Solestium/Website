46a7f3690ae15849d276982ad62a171a29004cac
Jurre de Jongh <jurre@solestium.nl>
2019-06-12 10:10:11 +0200

    initial commit

---
+++ pages/projects/gkdp/index.js
@@ -0,0 +1,21 @@
+function Home() {
+  return (
+    <div className="container">
+      <div className="title">GKDP</div>
+
+      <style jsx>{`
+        .container {
+          padding-top: 2rem;
+          display: flex;
+          justify-content: center;
+        }
+
+        .title {
+          font-size: 3rem;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default Home;
